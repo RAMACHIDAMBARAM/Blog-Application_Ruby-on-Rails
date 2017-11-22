b67d3fe6eddbb4dfb0bc0cbe6650ee359cfbff31
RAMACHIDAMBARAM <rm.ramachidambaram@gmail.com>
2017-11-22 08:12:57 +0000

    Adding Basic Static authentication scheme to Add article and Delete comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+    http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
+  
   def new 
     @article = Article.new
   end
@@ -9,6 +11,7 @@ class ArticlesController < ApplicationController
   @article = Article.new(article_params)
  
   if @article.save
+   redirect_to @article
     
   else
     render 'new'
