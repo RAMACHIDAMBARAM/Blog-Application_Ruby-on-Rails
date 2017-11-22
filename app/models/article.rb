a458c64ded607d9a5981d33b9e884f58cf3194ef
RAMACHIDAMBARAM <rm.ramachidambaram@gmail.com>
2017-11-22 08:02:04 +0000

    Feature for Deleting a comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   
-  has_many :comments
+   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 } # ensure that all articles have a title that is at least five characters long.
                     
